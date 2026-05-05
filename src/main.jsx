8926a6047bd73161041340e462b2b787e1a208b2
Olamiji Bunday <oduboteolamiji@gmail.com>
2026-05-05 08:46:35 -0700

    new change

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
+  document.getElementById('root')
 )
+
